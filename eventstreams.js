0f640ebc03de39f804dcd21e5d62f4c58574e54a
kasraat <56202952+kasraat@users.noreply.github.com>
2022-09-27 15:10:29 -0400

    Update eventstreams.js

--- eventstreams.js
+++ eventstreams.js
@@ -28,14 +28,14 @@ if (process.platform === "darwin") {
 // Config options common to all clients
 var driver_options = {
   'metadata.broker.list': eventStreamsCredentials.kafka_brokers_sasl.join(','),
-  //'security.protocol': 'sasl_ssl',
+  'security.protocol': 'sasl_ssl',
   //'security.protocol': 'plaintext',
-  'security.protocol': 'sasl_plaintext',
+  //'security.protocol': 'sasl_plaintext',
   'ssl.ca.location': process.env.CA_LOCATION || ca_location,
   'sasl.mechanisms': 'PLAIN',
   'sasl.username': 'token',
   'sasl.password': eventStreamsCredentials.api_key,
-  'broker.version.fallback': '0.10.0',  // still needed with librdkafka 0.11.6 to avoid fallback to 0.9.0
+  //'broker.version.fallback': '0.10.0',  // still needed with librdkafka 0.11.6 to avoid fallback to 0.9.0
   'log.connection.close': false
 };
 var consumer_opts = {
