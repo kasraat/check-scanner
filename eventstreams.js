0b60bc885bd6ede77d2a6f2aa59d5d58c70ca183
kasra.a <kasra.a@ibm.com>
2022-09-28 12:31:18 -0400

    original

--- eventstreams.js
+++ eventstreams.js
@@ -27,8 +27,8 @@ if (process.platform === "darwin") {
 
 // Config options common to all clients
 var driver_options = {
-  //'metadata.broker.list': eventStreamsCredentials.kafka_brokers_sasl.join(','),
-  'metadata.broker.list': eventStreamsCredentials.kafka_http_url,
+  'metadata.broker.list': eventStreamsCredentials.kafka_brokers_sasl.join(','),
+  //'metadata.broker.list': eventStreamsCredentials.kafka_http_url,
   'security.protocol': 'sasl_ssl',
   //'security.protocol': 'plaintext',
   //'security.protocol': 'sasl_plaintext',
@@ -38,12 +38,6 @@ var driver_options = {
   'sasl.password': eventStreamsCredentials.api_key,
   'broker.version.fallback': '0.10.0',  // still needed with librdkafka 0.11.6 to avoid fallback to 0.9.0
   'log.connection.close': false,
-  //'sasl.jaas.config': 'org.apache.kafka.common.security.plain.PlainLoginModule required username="token" password="hXe076FKfgUQYuCu9EQc8N7fswbzz6XvqIqfKcozpKER"',
-  // 'security.protocol': 'SASL_SSL',
-  // 'sasl.mechanism': 'PLAIN',
-  // 'ssl.protocol': 'TLSv1.2',
-  // 'ssl.enabled.protocols': 'TLSv1.2',
-  // 'ssl.endpoint.identification.algorithm': 'HTTPS'
 };
 var consumer_opts = {
   'client.id': 'pubsub-consumer',
